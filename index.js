b0d5f7f35cdad4f103e7bda4483f990a4509fa1d
DrVonDevious <drvondevious@gmail.com>
2020-03-07 14:57:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+
+document.querySelector('main').remove()
+var newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Caleb is the champion'
+

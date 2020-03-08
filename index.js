8041c7c335b90705c8d7353e77d7a34e0c0ba275
vienpham2019 <vienpham2019@gmail.com>
2020-03-08 11:55:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+let newHeader = document.createElement("h1")
+// newHeader.setAttribute("id","victory")
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion"
